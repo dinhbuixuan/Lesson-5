9c0b9e67e816306c2e3e7d3174490c89e0ec6ae1
dinhbuixuan <dinhbxk53@gmail.com>
2023-05-25 22:53:05 +0700

    bai thuc hanh 1

---
+++ demo.js
@@ -0,0 +1,47 @@
+// function tinhdiem3mon() {
+//     let t = 5;
+//     let v = 9;
+//     let a = 6;
+
+//     let total = (t + v) * 2 + a;
+
+//     if(total >= 30){
+//         alert('Pass');
+//     }else{
+//         alert('Fail')
+//     }
+
+// }
+// tinhdiem3mon();
+
+// let t = Number(prompt("Điểm toán của bạn là bao nhiêu?"));
+// let v = Number(prompt("Điểm văn của bạn là bao nhiêu?"));
+// let a = Number(prompt("Điểm anh của bạn là bao nhiêu?"));
+// let final = 2 * (t + v) + a;
+// if(final >= 30) {
+//     alert("Bạn đỗ cmnr");
+// } else {
+//     alert("Bạn trượt cmnr");
+// }
+
+// Công thức tính số ngày bạn đã sống
+
+// let t = prompt("Bạn năm nay bao nhiêu tuổi?");
+// let days = t*365;
+// document.write("Bạn đã sống được " + days + " ngày");
+
+// + Công thức tính chu vi và diện tích của đường tròn
+
+// let r = Number(prompt("Bán kính của đường tròn là:"));
+// const Pi = 3.14;
+// let C = 2*r*Pi;
+// let S = r*r*Pi;
+// alert("Chu vi của hình tròn là: " + C + " mét");
+// alert("Diện tích của hình tròn là: " + S + " mét vuông");
+
+// Công thức tính lãi suất ngân hàng
+
+// let t = Number(prompt("Số tiền bạn gửi vào ngân hàng là:"));
+// let n = Number(prompt("Số năm mà bạn muốn gửi là:"));
+// let monney = t + (6.9 / 100 * t * n);
+// alert("Số tiền mà bạn nhận được sau " + n + " năm là " + monney + " triệu");
